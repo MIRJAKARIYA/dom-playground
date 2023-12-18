5ca918e6b24a62e5e793e181b3f91c76c867ad87
MIRJAKARIYA <mirjakariya239@gmail.com>
2023-12-18 16:56:47 +0600

    modal done

--- src/components/Modal/CustomModal.jsx
+++ src/components/Modal/CustomModal.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Modal from "react-modal";
 
-const CustomModal = ({ modalIsOpen, setIsOpen, testCase, setTestCase }) => {
+const CustomModal = ({ modalIsOpen, setIsOpen, testCases, setTestCases }) => {
   let subtitle;
   const customStyles = {
     content: {
@@ -24,9 +24,22 @@ const CustomModal = ({ modalIsOpen, setIsOpen, testCase, setTestCase }) => {
   }
 
   const [eventChecked, setEventChecked] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const test = {
+      isEvent: false,
+      event: null,
+      event_selector: null,
+      what_selector_to_check: e.target.whichSelectorToCheck.value,
+      what_to_check: e.target.whatToCheck.value,
+    }
+    if(e.target.eventChecked.checked){
+      test.isEvent = true;
+      test.event = e.target.eventSelect.value;
+      test.event_selector = e.target.EventTriggeringSelector.value;
+    }
+    setTestCases([...testCases,test])
 
-  const handleEventSelect = (e) => {
-    console.log(e.target.value);
   };
 
   return (
@@ -39,9 +52,10 @@ const CustomModal = ({ modalIsOpen, setIsOpen, testCase, setTestCase }) => {
         contentLabel="Example Modal"
       >
         <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Add Test Case</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input
             type="checkbox"
+            name="eventChecked"
             onChange={(e) => setEventChecked(e.target.checked)}
             checked={eventChecked}
           />{" "}
@@ -49,7 +63,7 @@ const CustomModal = ({ modalIsOpen, setIsOpen, testCase, setTestCase }) => {
           {eventChecked && (
             <div>
               Select Event:
-              <select defaultValue={"click"} name="eventSelect" id="">
+              <select defaultValue="click" name="eventSelect" id="">
                 <option value="click">click</option>
                 <option value="dblclick">double click</option>
                 <option value="mouseenter">mouse enter</option>
@@ -62,11 +76,11 @@ const CustomModal = ({ modalIsOpen, setIsOpen, testCase, setTestCase }) => {
           )}
           <div>
           What selector to check: <input name="whichSelectorToCheck" type="text"/>
-         
           </div>
           <div>
           Which property to check: <input name="whatToCheck" type="text"/>
           </div>
+          <button type="submit">Add Test Case</button>
         </form>
         <button onClick={() => setIsOpen(false)}>close</button>
       </Modal>
--- src/pages/AddProblem/AddProblem.jsx
+++ src/pages/AddProblem/AddProblem.jsx
@@ -12,15 +12,11 @@ const AddProblem = () => {
 
   const [html, setHtml] = useState("");
   const [js, setJs] = useState("");
-  const [testCase, setTestCase] = useState({
-    isEvent: true,
-    event: "",
-    event_selector: "",
-    what_selector_to_check: "",
-    what_to_check: "",
-  });
+  const [testCases, setTestCases] = useState([]);
 
-
+  useEffect(() => {
+    console.log(testCases);
+  }, [testCases]);
 
   const onChangeJs = useCallback((value, viewUpdate) => {
     setJs(value);
@@ -33,32 +29,72 @@ const AddProblem = () => {
     console.log(html, js);
   }, [html, js]);
 
-
   //handle test case modal
   const [modalIsOpen, setIsOpen] = useState(false);
 
-  const handleSubmit = (e) => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({
+      challenge_name: e.target.challengeName.value,
+      challenge_des: e.target.challengeDes.value,
+      html: html,
+      js: js,
+    });
+  };
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          challenge Name: <input type="text" />
-        </div>
-        <div>
-          challenge Name: <input type="text" />
-        </div>
-        <div>
-          Initial Html
-          <CodeEditor onChange={onChangehtml}></CodeEditor>
-        </div>
-        <div>
-          Initial Js
-          <CodeEditor onChange={onChangeJs}></CodeEditor>
-        </div>
-      </form>
-      Add Test Cases:
-      <button onClick={()=>setIsOpen(true)}>Open Modal</button>
-      <CustomModal modalIsOpen={modalIsOpen} setIsOpen={setIsOpen} testCase={testCase} setTestCase={setTestCase}></CustomModal>
+    <div style={{display:"flex"}}>
+      <div style={{width:"48%",border:"1px solid red"}}>
+        <form onSubmit={handleSubmit}>
+          <div>
+            challenge Name: <input name="challengeName" type="text" />
+          </div>
+          <div>
+            challenge Des: <input name="challengeDes" type="text" />
+          </div>
+          <div>
+            Initial Html
+            <CodeEditor onChange={onChangehtml}></CodeEditor>
+          </div>
+          <div>
+            Initial Js
+            <CodeEditor onChange={onChangeJs}></CodeEditor>
+            <button type="submit">Add problem</button>
+          </div>
+        </form>
+        Add Test Cases:
+        <button onClick={() => setIsOpen(true)}>Open Modal</button>
+        <CustomModal
+          modalIsOpen={modalIsOpen}
+          setIsOpen={setIsOpen}
+          testCases={testCases}
+          setTestCases={setTestCases}
+        ></CustomModal>
+      </div>
+
+      {/* input pattern */}
+      <div style={{width:"48%",border:"1px solid green"}}>
+        {
+          testCases.map((test,index)=>{
+            return <div style={{backgroundColor:"red",width:"100%",marginBottom:"10px"}}>
+              {
+                test.isEvent && <>
+                <p>Event: {test.event}</p>
+              <p>Event Selector: {test.event_selector}</p>
+                </>
+                
+
+
+
+              }
+              <p>what selector to check: {test.what_selector_to_check}</p>
+              <p>what to check: {test.what_to_check}</p>
+              
+
+
+            </div>
+          })
+        }
+      </div>
     </div>
   );
 };
